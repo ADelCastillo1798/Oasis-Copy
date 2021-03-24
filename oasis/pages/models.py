3bd2e461d9ea7e3aa7c35eb6a9fbf680a41d1cb2
Amalia Riegelhuth <riegelhu@bc.edu>
2021-03-24 14:34:08 -0400

    updating django app, run on local server and add /pages to the end to see if it runs

---
+++ oasis/pages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=500)
+    date_added = models.DateField('date added')
+
+    def __str__(self):
+        return self.name
