0e74701f75cb9da522ae54c91262d95326f5b5e2
Amalia Riegelhuth <riegelhu@bc.edu>
2021-04-07 19:39:15 -0400

    got login to work

--- oasis/pages/forms.py
+++ oasis/pages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -9,6 +9,7 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+
 
 ###old form replaced by above
 class RegistrationForm(UserCreationForm):
--- oasis/pages/views.py
+++ oasis/pages/views.py
@@ -33,33 +33,24 @@ def clientcreation(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            # raw_password = form.cleaned_data.get('password1')
-            # user = authenticate(username=username, password=raw_password)
-            # login(request)
             return redirect('/')
     else:
         form = UserRegistrationForm()
     return render(request, 'clientcreation.html', {'form': form})
 
-def login(request):
+def loginview(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request, f"You are now logged in as {username}")
-                return redirect('/')
-            else:
-                messages.error(request, "Invalid username or password.")
-        else:
-            messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
-    return render(request = request,
-                    template_name = "login.html",
-                    context={"form":form})
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+   
 
 def landingpage(request):
     return render(request, 'LandingPage.html')
